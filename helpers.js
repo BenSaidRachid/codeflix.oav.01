867f21435c005d753c2cd1553f97397932833d16
mualimOfQuran <info.mualim@gmail.com>
2019-10-20 15:08:26 +0200

    refactoring

--- helpers.js
+++ helpers.js
@@ -1,15 +1,29 @@
 const path = require("path");
 const fs = require("fs");
+const ini = require("./parse_ini");
+const env = require("./parse_env");
 
 exports.format_date = function format_date(date) {
     if(date < 10) return "0" + date;
     return date;
 };
 
-exports.check_extension = function check_extension(filename) {
-    if(path.extname(filename) === ".ini") return 1;
-    else if(filename.replace(/(^.*)(\.env$)/g, "$2") === ".env") return 2;
-    else return -1;
+exports.convert_obj_to_json = function convert_obj_to_json(filename, file_type) {
+    let json_stringify = "";
+    const content = fs.readFileSync(filename, "utf-8");
+    switch (file_type) {
+        case 1:
+            json_stringify = ini(content);
+            break;
+        case 2:
+            json_stringify = env(content);
+            break;
+        default:
+            console.log("Error extension: the only extensions accepted are ini and env.");
+            process.exit(-1);
+            break;
+    }
+    return json_stringify;
 };
 
 exports.create_json_file = function create_json_file(json, date, type) {
@@ -25,4 +39,10 @@ exports.create_json_file = function create_json_file(json, date, type) {
         console.error("An error occurred\n",err);
         process.exit(-1);
     }
+};
+
+exports.check_extension = function check_extension(filename) {
+    if(path.extname(filename) === ".ini") return 1;
+    else if(filename.replace(/(^.*)(\.env$)/g, "$2") === ".env") return 2;
+    else return -1;
 };
--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 const fs = require("fs");
-const ini = require("./parse_ini");
-const env = require("./parse_env");
 const helper = require("./helpers");
 
 const args = process.argv.slice(2);
@@ -17,23 +15,9 @@ if (!fs.existsSync(filename)) {
     process.exit(-1)
 }
 
-const content = fs.readFileSync(filename, "utf-8");
 const date = new Date();
-let date_name = date.getFullYear() + "" + helper.format_date(date.getMonth()) + "" + helper.format_date(date.getDate())
+const date_name = date.getFullYear() + "" + helper.format_date(date.getMonth()) + "" + helper.format_date(date.getDate())
     + "" + helper.format_date(date.getHours()) + "" + helper.format_date(date.getMinutes()) + "" + helper.format_date(date.getSeconds());
-let json_stringified = "";
 const file_type = helper.check_extension(filename);
-switch (file_type) {
-    case 1:
-        json_stringified = ini(content);
-        break;
-    case 2:
-        json_stringified = env(content);
-        break;
-   default:
-       console.log("Error extension: the only extensions accepted are ini and env");
-       process.exit(-1);
-       break;
-}
-
-helper.create_json_file(json_stringified, date_name, file_type);
+const json_stringify = helper.convert_obj_to_json(filename, file_type);
+helper.create_json_file(json_stringify, date_name, file_type);
